2f43cf0cea1a389e5528e11ec6091bed73648ecf
Ahmad Osman <ahmad.osman.2009@gmail.com>
2019-06-01 11:27:02 -0400

    Covert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ahmad Osman on 5/31/19.
+//  Copyright © 2019 Mido Apps. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
